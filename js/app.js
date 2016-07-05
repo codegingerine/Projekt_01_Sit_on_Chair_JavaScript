dbd136a22c10f3e616b55268209cacb06ce4f1d2
codegingerine <urskosik@gmail.com>
2016-07-05 19:15:31 +0200

    JS - Slider
    reszta plików html, css .....

--- js/app.js
+++ js/app.js
@@ -1,15 +1,45 @@
 document.addEventListener("DOMContentLoaded", function(){
 
 
+// ----- Slider -----
+
+var nextSlaidBtn = document.querySelector('.slider-btn-right');
+var sliderPhotos = document.querySelectorAll('.slider-photo li');
+var prevSlideBtn = document.querySelector('.slider-btn-left');
+
+var index = 0;
+	sliderPhotos[index].classList.add('visible'); 
+	nextSlaidBtn.addEventListener("click", function(event) {
+		var counter = 0;
+		var counterValue = counter + 1;      
+		sliderPhotos[index].classList.remove('visible');
+
+		if (index === sliderPhotos.length-1) {
+			index = 0;
+		} else {
+			index++;
+		}
+		sliderPhotos[index].classList.add('visible');
+		});
+
+	prevSlideBtn.addEventListener("click", function(event) {
+	sliderPhotos[index].classList.remove('visible');
+
+		if (index === 0) {
+			index = sliderPhotos.length-1;
+		} else {
+			index--;
+		}
+	sliderPhotos[index].classList.add('visible');
+	});
+
 // ----- Show Element - DropDown menu -----
 var dropDownMenu = document.querySelectorAll('nav ol > li > ul');
-console.log(dropDownMenu);
 	for (var i = 0; i < dropDownMenu.length; i++) {
 		dropDownMenu[i].addEventListener('mouseup', function(showElement) { 
 			this.style.display = 'block';
 		});
 	}	
-
 
 // ----- Hide Element ----- 
 var bgTransparent = document.querySelectorAll('.bg-transparent');
@@ -19,13 +49,4 @@ var bgTransparent = document.querySelectorAll('.bg-transparent');
 		});
 	}
 
-
-
-
-
-
-
-
-
-//****************************************
 });
