f717bb0c180cdac70d07849c964574b9e9f2b733
codegingerine <urskosik@gmail.com>
2016-07-05 17:02:29 +0200

    Show DropDown menu

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
 
 
+// ----- Show Element - DropDown menu -----
+var dropDownMenu = document.querySelectorAll('nav ol > li > ul');
+console.log(dropDownMenu);
+	for (var i = 0; i < dropDownMenu.length; i++) {
+		dropDownMenu[i].addEventListener('mouseup', function(showElement) { 
+			this.style.display = 'block';
+		});
+	}	
+
 
 // ----- Hide Element ----- 
 var bgTransparent = document.querySelectorAll('.bg-transparent');
