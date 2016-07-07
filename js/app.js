16dc39ef5a142219be6bdd46692acbd76603f20c
codegingerine <urskosik@gmail.com>
2016-07-07 13:39:51 +0200

    Menu poprawione - nie chowa się subMenu!!!!

--- js/app.js
+++ js/app.js
@@ -2,16 +2,17 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 // ----- Show Element - DropDown menu -----
-var listElementsVisible = document.querySelectorAll('nav ol > li > a ');
+var listElementsVisible = document.querySelectorAll('nav ol > li');
 
-	for (var i = 0; i < listElementsVisible.length; i++) {
+	for (var i=0; i < listElementsVisible.length; i++) {
 		listElementsVisible[i].addEventListener('mouseover', function(show) {
-			this.nextElementSibling.style.display = 'block';
+			this.firstElementChild.nextElementSibling.style.display = 'block';
 		});
+
 		listElementsVisible[i].addEventListener('mouseout', function(hide) {
-			this.nextElementSibling.style.display = 'none';
+			this.firstElementChild.nextElementSibling.style.display = 'none';
 		});
-	}	
+	}
 
 // ----- Slider -----
 var nextSlaidBtn = document.querySelector('.slider-btn-right');
@@ -49,6 +50,57 @@ var bgTransparent = document.querySelectorAll('.bg-transparent');
 			this.style.display = 'none';
 		});
 	}
+
+
+// ----- Application options ----- 
+
+var appArrows = document.querySelectorAll('.list_arrow');
+// console.log(appArrows);
+
+//v1 po dodaniu drugiegi eventu, nic nie działa
+	for (var i = 0; i < appArrows.length; i++) {
+		appArrows[i].addEventListener('click', function(show) {
+			this.nextElementSibling.style.display = 'block';	
+		});
+
+		// appArrows[i].addEventListener('click', function(hide) {
+		// 	this.nextElementSibling.style.display = 'none';
+		// });	
+
+	}	
+
+
+//v 2 - tylko jeden element reaguje na oba eventy
+
+	// for (var i = 0; i < appArrows.length; i++) {
+	// 	appArrows[i].addEventListener('click', function(show) {
+	// 		this.nextElementSibling.style.display = 'block';	
+
+	// 		for (var i = 0; i < appArrows.length; i++) {
+	// 			appArrows[i].addEventListener('click', function(hide) {
+	// 				this.nextElementSibling.style.display = 'none';
+	// 			});	
+	// 		}		
+		
+	// 	});
+	// }	
+
+
+// v.3 drugi event nie działa
+
+	// for (var i = 0; i < appArrows.length; i++) {
+	// 	appArrows[i].addEventListener('click', function(show) {
+	// 		if (this.nextElementSibling.style.display = 'none') {
+	// 		this.nextElementSibling.style.display = 'block';
+	// 		} else {
+	// 			this.nextElementSibling.style.display = 'none';
+	// 		}
+	// 	});
+	// }	
+
+
+
+	
 
 
 
