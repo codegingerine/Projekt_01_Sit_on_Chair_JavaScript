1eb8810a2dadb76f101cfbb55289188d04b63549
codegingerine <urskosik@gmail.com>
2016-07-16 16:33:10 +0200

    Hide category list: 1. click arrow 2. click category item
    Hide category list on clicking a chosen category item inside the list

--- js/app.js
+++ js/app.js
@@ -1,84 +1,96 @@
 document.addEventListener("DOMContentLoaded", function(){
 
 
-// ----- Show Element - DropDown menu -----
-var listElementsVisible = document.querySelectorAll('nav ol > li');
+	// ----- DropDown menu - Show/Hide Element -----
+	var listElementsVisible = document.querySelectorAll('nav ol > li');
 
-function DropdownMenu() {
-	for (var i=0; i < listElementsVisible.length; i++) {
-		listElementsVisible[i].addEventListener('mouseover', function(show) {
-			this.firstElementChild.nextElementSibling.style.display = 'block';
-		});
-		listElementsVisible[i].addEventListener('mouseout', function(hide) {
-			this.firstElementChild.nextElementSibling.style.display = 'none';
-		});
+	function DropdownMenu() {
+		for (var i=0; i < listElementsVisible.length; i++) {
+			listElementsVisible[i].addEventListener('mouseover', function(show) {
+				this.firstElementChild.nextElementSibling.style.display = 'block'; //embedded ul visible
+			});
+			listElementsVisible[i].addEventListener('mouseout', function(hide) {
+				this.firstElementChild.nextElementSibling.style.display = 'none'; //embedded ul back to invisible
+			});
+		};
 	};
-};
-DropdownMenu();
+	DropdownMenu();
 
-// ----- Slider -----
-var nextSlaidBtn = document.querySelector('.slider-btn-right');
-var sliderPhotos = document.querySelectorAll('.slider-photo li');
-var prevSlideBtn = document.querySelector('.slider-btn-left');
+	// ----- Section Slider -----
+	var nextSlaidBtn = document.querySelector('.slider-btn-right');
+	var sliderPhotos = document.querySelectorAll('.slider-photo li');
+	var prevSlideBtn = document.querySelector('.slider-btn-left');
 
-function mainSlider() {
-	var index = 0;
-	sliderPhotos[index].classList.add('visible'); 
-		nextSlaidBtn.addEventListener("click", function(event) {
-			sliderPhotos[index].classList.remove('visible');
-			if (index === sliderPhotos.length-1) {
-				index = 0;
-			} else {
-				index++;
-			};
-			sliderPhotos[index].classList.add('visible');
-		});
-		prevSlideBtn.addEventListener("click", function(event) {
-			sliderPhotos[index].classList.remove('visible');
-			if (index === 0) {
-				index = sliderPhotos.length-1;
-			} else {
-				index--;
-			}
-			sliderPhotos[index].classList.add('visible');
-		});
-};
-mainSlider();
-
-// ----- Hide Element ----- 
-var bgTransparent = document.querySelectorAll('.bg-transparent');
-
-function titleHide() {
-	for (var i = 0; i < bgTransparent.length; i++) {
-		bgTransparent[i].addEventListener('mouseover', function(hideElement) { 
-			this.style.display = 'none';
-		});
+	function mainSlider() {
+		var index = 0;
+		sliderPhotos[index].classList.add('visible'); 
+			nextSlaidBtn.addEventListener("click", function(event) {
+				sliderPhotos[index].classList.remove('visible');
+				if (index === sliderPhotos.length-1) {
+					index = 0;
+				} else {
+					index++;
+				};
+				sliderPhotos[index].classList.add('visible');
+			});
+			prevSlideBtn.addEventListener("click", function(event) {
+				sliderPhotos[index].classList.remove('visible');
+				if (index === 0) {
+					index = sliderPhotos.length-1;
+				} else {
+					index--;
+				}
+				sliderPhotos[index].classList.add('visible');
+			});
 	};
-};
-titleHide();
+	mainSlider();
 
-// ----- Application options ----- 
-var appArrows = document.querySelectorAll('.list_arrow');
+	// ----- Section Offer - Hide title bars ----- 
+	var bgTransparent = document.querySelectorAll('.bg-transparent');	//transparent bars with box title
 
-function categories() {
-	for (var i = 0; i < appArrows.length; i++) {
-		appArrows[i].addEventListener('click', function(show) {
-			this.nextElementSibling.style.display = 'block';	
+	function titleBarsHide() {
+		for (var i = 0; i < bgTransparent.length; i++) {
+			bgTransparent[i].addEventListener('mouseover', function(hideElement) { 
+				this.style.display = 'none';
+			});
+		};
+	};
+	titleBarsHide();
 
-			for (var i = 0; i < appArrows.length; i++) {
-				appArrows[i].addEventListener('click', function(hide) {
-		     		var list = this.nextElementSibling; // cache element
-		     		if ( list.style.display != 'none' &&  list.style.display != "") { //spr.
-		       			list.style.display = 'none';
-		     		}	else {
-		      				list.style.display = 'block';
-		     		};
-				});
-			};
-		});
-	};	
-};
-categories();
+	// ----- Section Buy Chair Form ----- 
+	var appArrows = document.querySelectorAll('.list_arrow');
+	var categories = document.querySelectorAll(".list_panel li"); //li categories
+	console.log(categories)
+
+	// Category DropDown Lists 
+	function categoriesLists() {
+		for (var i = 0; i < appArrows.length; i++) {
+			// Show category list on arrow click
+			appArrows[i].addEventListener('click', function(show) { 
+				var list = this.nextElementSibling; 		// ul - category list, cache element	
+				list.style.display = 'block';	
+				// Hide category list on arrow click
+				for (var i = 0; i < appArrows.length; i++) {
+					appArrows[i].addEventListener('click', function(hide) {
+				   		if ( list.style.display != 'none' &&  list.style.display != "") { 
+				   			list.style.display = 'none';
+				   		} else {
+				   			list.style.display = 'block';
+				   		};
+					});
+				};
+			});
+		};
+		// Hide category list on clicking a chosen category item inside the list 
+		for (var i = 0; i < categories.length; i++) {
+			categories[i].addEventListener('click', function(hide) {
+				this.parentElement.style.display = 'none';
+			});	
+		};
+	};
+	categoriesLists();
+
+
 
 
 });
